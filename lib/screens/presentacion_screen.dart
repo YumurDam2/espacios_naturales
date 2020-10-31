27a6c17b7540cb06b85f97d2fd6ef659468d62a9
imonleonedam1 <imonleonedam1@gamil.com>
2020-10-31 19:58:39 +0100

    ZonasGeograficas

---
+++ lib/screens/presentacion_screen.dart
@@ -0,0 +1,25 @@
+//import 'package:espacios_naturales/screens/principal_screen.dart';
+import 'package:espacios_naturales/screens/zonas_geograficas_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+class PresentacionScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new SplashScreen(
+        seconds: 10,
+        navigateAfterSeconds: ListaZonasGeograficasScreen(),
+        title: new Text(
+          'Espacios Naturales',
+          style: new TextStyle(fontStyle: FontStyle.italic, fontSize: 30.0),
+        ),
+        image: new Image.network(
+            'http://www.navarra.es/NR/rdonlyres/50841333-22E5-431E-873F-51AC3BEC30EB/54817/2804cu60b.jpg'),
+        backgroundColor: Colors.white,
+        styleTextUnderTheLoader: new TextStyle(),
+        photoSize: 120.0,
+        onClick: () => print("Flutter Spain"),
+        loadingText: Text("Embarcando..."),
+        loaderColor: Colors.red);
+  }
+}
---
+++ lib/screens/principal_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AfterSplash extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+          title: new Text("Welcome In SplashScreen Package"),
+          automaticallyImplyLeading: false),
+      body: new Center(
+        child: new Text(
+          "Done!",
+          style: new TextStyle(fontWeight: FontWeight.bold, fontSize: 30.0),
+        ),
+      ),
+    );
+  }
+}
