1da44965bcdc5cfa901576acd3779ebc918a65c3
imonleonedam1 <imonleonedam1@gamil.com>
2020-11-06 22:27:05 +0100

    TipoEspaciosScreen y TipoSeleccionadoScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:espacios_naturales/screens/presentacion_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:splashscreen/splashscreen.dart';
 
 void main() => runApp(MyApp());
@@ -7,9 +8,8 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       home: PresentacionScreen(),
-
     );
   }
 }
--- lib/screens/zonas_geograficas_screen.dart
+++ lib/screens/zonas_geograficas_screen.dart
@@ -1,27 +1,58 @@
+import 'package:espacios_naturales/screens/tipos_espacios_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:espacios_naturales/providers/menu_provider.dart';
+import 'package:get/get.dart';
 
 class ListaZonasGeograficasScreen extends StatelessWidget {
-  final listaZonas = ['Pirineos', 'Cuenca de Pamplona', 'Zona Media', 'Ribera'];
+  Map<String, Object> args = new Map<String, Object>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Zonas Geográficas"),
+        centerTitle: true,
+        backgroundColor: Colors.lightGreen,
       ),
-      body: ListView(
-        children: _listaZonas(),
-      ),
+      body: _lista(context),
     );
   }
 
-  List<Widget> _listaZonas() {
-    List<Widget> lst = [];
-    listaZonas.forEach((zona) {
-      final tile = new ListTile(
-        title: Text(zona),
+  Widget _lista(BuildContext context) {
+    return FutureBuilder(
+      future: menuProvider.cargarZonas(),
+      initialData: [],
+      builder: (context, snapshot) {
+        return ListView(
+          children: _listaZonas(context, snapshot.data),
+        );
+      },
+    );
+  }
+
+  List<Widget> _listaZonas(BuildContext context, List<String> data) {
+    final List<Widget> lst = [];
+
+    data.forEach((element) {
+      final w = ListTile(
+        title: Text(element),
+        leading: new Icon(Icons.eco_outlined, color: Colors.green),
+        trailing: Icon(
+          Icons.arrow_right,
+          color: Colors.green,
+        ),
+        onTap: () {
+          args['zona'] = element;
+          Get.offAll((ListaTiposEspaciosNaturalesScreen()), arguments: args);
+        },
       );
-      lst.add(tile);
-      lst.add(Divider());
+      lst.add(w);
+      lst.add(Divider(
+        height: 50,
+        thickness: 2,
+        indent: 100,
+        endIndent: 100,
+        color: Colors.green,
+      ));
     });
     return lst;
   }
